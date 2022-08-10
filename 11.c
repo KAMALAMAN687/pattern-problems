6e93143f077b976574456858a299d89abe71e6f5
Aman Kamal <109606739+KAMALAMAN687@users.noreply.github.com>
2022-08-10 15:32:22 +0530

    Create 11.c

---
+++ 11.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+int main()
+{
+    int i,j,k;
+    for(i=1;i<=5;i++)
+    {k='A';
+        for(j=1;j<=9;j++)
+        {
+            if(j>=6-i&&j<=4+i)
+            { 
+             if(j<=5)
+            {
+                printf("%c",k);
+                k++;
+            }
+             else
+             {
+                k--;
+                printf("%c",k-1);
+                
+                
+             } 
+                
+            }
+            else
+            {
+                printf(" ");
+            }
+        }
+        printf("\n");
+    }
+
+}
